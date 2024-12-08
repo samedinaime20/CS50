62b9814a90381bff61a21733ea111ffc7a1f5f74
samedinaime20 <samedinaime20@stud.ase.ro>
2024-12-08 15:52:48 +0200

    .

---
+++ CS50 Lecture0.py
@@ -0,0 +1,27 @@
+#name = input("What's your name? ").strip().title()
+#it will print hello and yor name
+#it will remove whitespace from str and it will capitalize the name
+#first, last = name.split()
+#print("Hello",first, sep=", ", end="!\n")
+#these are positional parameters
+#print(f"Ur welcome, {last}!")
+
+#-------------------------------------------------------
+
+#x=float(input("what's x? "))
+#y=float(input("what's y? "))
+#z=x/y
+#print(f"{z:.2f}")
+
+#-------------------------------------------------------
+
+#def main():
+    #x = int(input("Enter a number: "))
+    #print("x at power 3 is ",square(x))
+
+
+#def square(n):
+    #return pow(n,3)
+
+#main()
+#-------------------------------------------------------
---
+++ CS50 Lecture1.py
@@ -0,0 +1,39 @@
+
+#y= int(input("Enter y: "))
+
+#if x<y:
+    #print("x is less than y")
+#elif x>y:
+    #print("x is greater than y")
+#else:
+    #print("x and y are equal")
+
+#if x!=y:
+    #print("x and y are not equal")
+#else:
+    #print("x and y are equal")
+
+#score = int(input("Enter your score: "))
+#if score >= 90 and score <= 100:
+#    print("A")
+#elif score >= 80 and score < 90:
+#    print("B")
+#elif score >= 70 and score < 80:
+#    print("C")
+#elif score >= 60 and score < 70:
+#    print("D")
+#else:
+#    print("F")
+
+
+#name = input("Enter your name: ")
+
+#match name:
+#    case "Harry" | "Hermione" | "Ron":
+#        print("Griffindor")
+#    case "Draco":
+#        print("Slytherine")
+#    case _:
+#        print("WHO?")
+
+
