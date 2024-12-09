744eba379fc327b77112bd6b28b574e2be85f11d
samedinaime20 <samedinaime20@stud.ase.ro>
2024-12-09 22:48:56 +0200

    09.12.2024

--- CS50 Lecture2.py
+++ CS50 Lecture2.py
@@ -70,13 +70,13 @@
 #     print("?"*width)
 # main()
 
-def main():
-    print_square(3)
-
-def print_square(size):
-    for i in range(size):
-        print_row(size)
-
-def print_row(width):
-    print('#' * width)
-main()
+# def main():
+#     print_square(3)
+#
+# def print_square(size):
+#     for i in range(size):
+#         print_row(size)
+#
+# def print_row(width):
+#     print('#' * width)
+# main()
